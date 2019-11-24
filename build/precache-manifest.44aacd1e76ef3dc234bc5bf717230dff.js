842e809bd3c4082adb5452399690ab5d9bbf6b50
ZackMinott <zacharyminott1997@gmail.com>
2019-11-24 10:07:03 -0800

    adding monster files

---
+++ build/precache-manifest.44aacd1e76ef3dc234bc5bf717230dff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5d6e75f1b300d58f44302cb96eed06",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a1ab8418c7d96f8af6f5",
+    "url": "/monsters-rolodex/static/css/main.9e624027.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "a1ab8418c7d96f8af6f5",
+    "url": "/monsters-rolodex/static/js/main.dbc56f0e.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
